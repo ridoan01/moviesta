737ba5772dc624050c85c426c01d548853f9a78b
ridoan <ridoan@techgrid.co>
2019-10-31 14:45:35 +0700

    Initial Commit

---
+++ Moviesta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moviesta
+//
+//  Created by Ridoan Wibisono on 31/10/19.
+//  Copyright © 2019 Ridoan Wibisono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
